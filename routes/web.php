0a23ccf8b12aca62e85813d2fd074ef519490818
Vladimir Struhar <vstruhar@gmail.com>
2024-05-08 17:44:51 +0200

    Added a redirect from root to dashboard

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,8 @@ use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
+Route::redirect('/', 'dashboard');
+
 Route::group(['middleware' => ['auth:sanctum']], function () {
     // Dashboard
     Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
