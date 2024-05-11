3458709a30d3dd4e1e493601cea53a128b6f9434
Vladimir Struhar <vstruhar@gmail.com>
2024-05-11 10:51:28 +0200

    removed unneeded column from companies table

--- resources/js/pages/Clients/Companies/TableRow.jsx
+++ resources/js/pages/Clients/Companies/TableRow.jsx
@@ -1,59 +1,66 @@
-import TableRowActions from "@/components/TableRowActions";
-import { money } from "@/utils/currency";
-import { Link } from "@inertiajs/react";
-import { Badge, Group, Table, Text } from "@mantine/core";
+import TableRowActions from '@/components/TableRowActions';
+import { Link } from '@inertiajs/react';
+import { Badge, Group, Table, Text } from '@mantine/core';
 
 export default function TableRow({ item }) {
   return (
     <Table.Tr key={item.id}>
       <Table.Td>
-        <Text fz="sm" fw={500}>
+        <Text
+          fz='sm'
+          fw={500}
+        >
           {item.name}
         </Text>
       </Table.Td>
       <Table.Td>
-        <Text fz="sm">{item.email}</Text>
-        <Text fz="xs" c="dimmed">
+        <Text fz='sm'>{item.email}</Text>
+        <Text
+          fz='xs'
+          c='dimmed'
+        >
           Email
         </Text>
       </Table.Td>
       <Table.Td>
-        <Text fz="sm" fw={500}>
-          {item.rate ? money(item.rate, item.currency.code, item.currency.decimals) : "-"}
-        </Text>
-      </Table.Td>
-      <Table.Td>
-        <Group gap="sm">
-          {item.clients.map((item) => (
-            <Link href={route("clients.users.edit", item.id)} key={item.id}>
-              <Badge variant="light" color="orange" tt="unset">
+        <Group gap='sm'>
+          {item.clients.map(item => (
+            <Link
+              href={route('clients.users.edit', item.id)}
+              key={item.id}
+            >
+              <Badge
+                variant='light'
+                color='orange'
+                tt='unset'
+              >
                 {item.name}
               </Badge>
             </Link>
           ))}
         </Group>
       </Table.Td>
-      {(can("edit client company") ||
-        can("archive client company") ||
-        can("restore client company")) && (
+      {(can('edit client company') ||
+        can('archive client company') ||
+        can('restore client company')) && (
         <Table.Td>
           <TableRowActions
             item={item}
-            editRoute="clients.companies.edit"
-            editPermission="edit client company"
-            archivePermission="archive client company"
-            restorePermission="restore client company"
+            editRoute='clients.companies.edit'
+            editPermission='edit client company'
+            archivePermission='archive client company'
+            restorePermission='restore client company'
             archive={{
-              route: "clients.companies.destroy",
-              title: "Archive company",
+              route: 'clients.companies.destroy',
+              title: 'Archive company',
               content: `Are you sure you want to archive this company?`,
-              confirmLabel: "Archive",
+              confirmLabel: 'Archive',
             }}
             restore={{
-              route: "clients.companies.restore",
-              title: "Restore company",
+              route: 'clients.companies.restore',
+              title: 'Restore company',
               content: `Are you sure you want to restore this company?`,
-              confirmLabel: "Restore",
+              confirmLabel: 'Restore',
             }}
           />
         </Table.Td>
