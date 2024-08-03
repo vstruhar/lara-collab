17271457b52c8190058aa267ffb1f9e149b5d93a
Vladimir Struhar <vstruhar@gmail.com>
2024-08-03 12:44:16 +0200

    fixed aside on opened project jumps down when task has a long name

--- resources/js/components/Label.jsx
+++ resources/js/components/Label.jsx
@@ -2,7 +2,7 @@ import { ColorSwatch, Group, Text } from "@mantine/core";
 
 export function Label({ name, color, size = 10, dot = true }) {
   return (
-    <Group gap={5} my={2}>
+    <Group gap={5} my={2} wrap="nowrap">
       {dot === true && <ColorSwatch color={color} size={size} />}
       <Text fz={size} tt="uppercase" c={color} fw={500}>
         {name}
